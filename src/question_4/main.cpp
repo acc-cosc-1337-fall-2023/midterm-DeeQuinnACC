087e2916fb8d9ad546cac056948bf9a4a008aef7
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-10-11 03:29:44 +0000

    question 4

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -1,6 +1,38 @@
+#include <iostream>
+#include <string>
+#include "question4.h"
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main()
 {
+    string input;
+    bool palindrome;
+
+    do
+    {
+        cout<<"\nPlease enter a word to check: ";
+        cin>>input;
+
+        palindrome = is_palindrome(input);
+
+        if(palindrome)
+        {
+            cout<<"\n'"<<input<<"' is a palindrome!";
+        }
+        else
+        {
+            cout<<"\n'"<<input<<"' is NOT a palindrome!";
+        }
+        
+        cout<<"\n\nTo exit the program, type 'exit'.";
+        cout<<"\nOtherwise, type anything to test another word.";
+        cout<<"\nPlease enter your choice: ";
+        cin>>input;
+    } while (input != "exit");
+    
 
     return 0;
 }
--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -1,5 +1,20 @@
-//
+#include "question4.h"
+
+using std::string;
+
 bool test_config()
 {
     return true;
+}
+
+bool is_palindrome(const string& word)
+{
+    string backwardsWord = "";
+
+    for(int i = word.length() - 1; i >= 0; i--)
+    {
+        char letter = word[i];
+        backwardsWord += letter;
+    }
+    return(word == backwardsWord);
 }
